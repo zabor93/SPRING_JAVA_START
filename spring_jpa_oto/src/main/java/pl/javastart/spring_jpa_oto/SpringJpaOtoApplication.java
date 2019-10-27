d2a66e052099bfe0c3616a01310bcfe4223c6af0
Jakub Zaboronek <jakubzaboronek@wp.pl>
2019-10-27 15:24:57 +0100

    Initial commit

---
+++ spring_jpa_oto/src/main/java/pl/javastart/spring_jpa_oto/SpringJpaOtoApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.spring_jpa_oto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaOtoApplication.class, args);
+    }
+
+}
---
+++ spring_jpa_oto/src/test/java/pl/javastart/spring_jpa_oto/SpringJpaOtoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.spring_jpa_oto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaOtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
