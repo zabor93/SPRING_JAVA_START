cfd432f4b009d2edffab3fd0f5624dbbc73e9f79
Jakub Zaboronek <jakubzaboronek@wp.pl>
2019-10-27 15:45:52 +0100

    Spring jpa many to one

---
+++ spring_jpa_otm/src/main/java/pl/javastart/spring_jpa_otm/SpringJpaOtmApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.spring_jpa_otm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaOtmApplication.class, args);
+    }
+
+}
---
+++ spring_jpa_otm/src/test/java/pl/javastart/spring_jpa_otm/SpringJpaOtmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.spring_jpa_otm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaOtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
